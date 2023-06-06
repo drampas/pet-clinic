896daf2ba581999f383b6911b4f42a977faf8223
drampas <drampasaggelos@gmail.com>
2023-06-06 15:06:03 +0300

    implemented find owners

--- pet-clini-web/src/main/java/drampas/springframework/petclinic/controllers/OwnerController.java
+++ pet-clini-web/src/main/java/drampas/springframework/petclinic/controllers/OwnerController.java
@@ -1,9 +1,13 @@
 package drampas.springframework.petclinic.controllers;
 
+import drampas.springframework.petclinic.model.Owner;
 import drampas.springframework.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class OwnerController {
@@ -15,9 +19,25 @@ public class OwnerController {
     }
 
     @RequestMapping({"/owners/index","/owners/index.html","/owners","/owners/find"})
-    public String ownerList(Model model){
-
-        model.addAttribute("owners",ownerService.findAll());
-        return "owners/index";
+    public String findOwners(Model model){
+        model.addAttribute("owner",new Owner());
+        return "owners/findOwners";
+    }
+    @GetMapping("/owners")
+    public String findForm(Owner owner, Model model){
+        if(owner.getLastName()==null){
+            owner.setLastName("");
+        }
+        List<Owner> result= ownerService.findAllByLastNameLike("%"+owner.getLastName()+"%");
+        if(result.isEmpty()){
+            return "owners/findOwners";
+        } else if (result.size()==1) {
+            owner = result.get(0);
+            model.addAttribute("owner",owner);
+            return "owners/ownerDetails";
+        }else{
+            model.addAttribute("owners",result);
+            return "owners/index";
+        }
     }
 }
